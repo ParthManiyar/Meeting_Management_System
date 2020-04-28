df5f52e569d9dfbf5abc848074b7a87a3e9ef698
Shaunak Vyas <shaunakvyas56@gmail.com>
2020-04-28 12:56:04 +0530

    added models

--- MMSApp/admin.py
+++ MMSApp/admin.py
@@ -1,6 +1,11 @@
+from django.apps import apps
 from django.contrib import admin
-from .models import *
+from django.contrib.admin.sites import AlreadyRegistered
 
-# Register your models here.
+app_models = apps.get_app_config('MMSApp').get_models()
 
-admin.site.register(CustomUser)
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
--- MMSApp/models.py
+++ MMSApp/models.py
@@ -1,11 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+#########
+
+class Venue(models.Model):
+
+    name = models.CharField(max_length=200)
+
+#########
+
+class Event(models.Model):
+    
+    start_time = models.TimeField()
+    end_time   = models.TimeField()
+    name       = models.CharField(max_length=200)
+    Venue      = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+#########
+
+class DailySchedule(models.Model):
+
+    date   = models.DateField()
+    events = models.ManyToManyField(Event)
+
+#########
+
+class Schedule(models.Model):
+
+    daily_schedules = models.ManyToManyField(DailySchedule)
+
+#########
+
 class CustomUser(User):
 
     class Meta:
         verbose_name = "CustomUser"
         verbose_name_plural = "CustomUsers"
+
+    schedule = models.ManyToManyField(Schedule)
 
     def __str__(self):
         return self.username
@@ -16,3 +49,59 @@ class CustomUser(User):
         super(CustomUser, self).save(*args, **kwargs)
 
 #########
+
+class Message(models.Model):
+
+    text   = models.CharField(max_length=1000)
+    writer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    time   = models.DateTimeField(auto_now=True)
+
+#########
+
+class ChatRoom(models.Model):
+
+    messages = models.ManyToManyField(Message)
+
+#########
+
+class Resource(models.Model):
+
+    rfile       = models.FileField()
+    owner       = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    upload_time = models.DateTimeField(auto_now=True)
+
+
+#########
+
+class Meeting(models.Model):
+
+    name         = models.CharField(max_length=200)
+    agenda       = models.CharField(max_length=500)
+    admin        = models.ManyToManyField(CustomUser,related_name="meeting_admin")
+    invitees     = models.ManyToManyField(CustomUser,related_name="invitee")
+    attendees    = models.ManyToManyField(CustomUser,related_name="attendee")
+    start_time   = models.DateTimeField()
+    end_time     = models.DateTimeField()
+    duration     = models.IntegerField() ##
+    venue        = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    resources    = models.ManyToManyField(Resource)
+    chatroom     = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+
+#########
+
+class Group(models.Model):
+
+    name         = models.CharField(max_length=200)
+    admins       = models.ManyToManyField(CustomUser,related_name="admin")
+    members      = models.ManyToManyField(CustomUser,related_name="member")
+    meetings     = models.ManyToManyField(Meeting,related_name="group")
+    created_date = models.DateTimeField()
+
+#########
+
+class Notification(models.Model):
+
+    title        = models.CharField(max_length=200)
+    content      = models.CharField(max_length=300)
+    target_users = models.ManyToManyField(CustomUser,related_name="notif")
